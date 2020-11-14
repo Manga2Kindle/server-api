1d1962933bcdf7def4c429aeaa67e95f7bda72c0
Eduardo Fernandez <yo@edufdezsoy.es>
2020-11-14 15:16:55 +0100

    added relations to manga returned from database

--- src/repositories/MangaRepository.ts
+++ src/repositories/MangaRepository.ts
@@ -4,10 +4,15 @@ import { Manga } from "../models/Manga";
 @EntityRepository(Manga)
 export class MangaRepository extends Repository<Manga> {
   findById(id: number): Promise<Manga | undefined> {
-    return this.findOne(id);
+    return this.findOne(id, {
+      relations: ["author"]
+    });
   }
 
   findByTitle(title: string): Promise<Manga[]> {
-    return this.find({ title: Like(title) });
+    return this.find({
+      where: { title: Like(title) },
+      relations: ["author"]
+    });
   }
 }
