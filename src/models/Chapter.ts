6781e880950b3f67e61b93f12a58e8bf1018f3cd
Euardo Fernandez <yo@edufdez.es>
2020-12-02 23:41:10 +0100

    Chapter entity

---
+++ src/models/Chapter.ts
@@ -0,0 +1,27 @@
+import { Example, Property, Required } from "@tsed/schema";
+import { Manga } from "./Manga";
+
+export class Chapter {
+  @Property()
+  @Required()
+  @Example("Manga")
+  public manga: Manga;
+
+  @Property()
+  @Example("The sword village")
+  public title?: string;
+
+  @Property()
+  @Required()
+  @Example(1, 64, 123.5)
+  public chapter: number;
+
+  @Property()
+  @Example(20)
+  public volume?: number;
+
+  @Property()
+  @Required()
+  @Example(24)
+  public pages: number;
+}
